51b7c30870307cdd896b628c561d76febe2a772a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-08-27 01:03:40 +0200

    add license text

--- ics_query/cli.py
+++ ics_query/cli.py
@@ -1,3 +1,18 @@
+# ics-query
+# Copyright (C) 2024 Nicco Kunzmann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """The command line interface."""
 
 from __future__ import annotations
@@ -6,9 +21,9 @@ import functools
 import os  # noqa: TCH003
 import sys
 import typing as t
+import zoneinfo
 
 import click
-import zoneinfo
 from icalendar.cal import Calendar, Component
 
 from . import parse
@@ -633,6 +648,7 @@ def between(
 
     """  # noqa: D301
     output.add_components(calendar.between(start, end))
+
 
 
 def main():
--- ics_query/tests/test_command_line.py
+++ ics_query/tests/test_command_line.py
@@ -1,3 +1,18 @@
+# ics-query
+# Copyright (C) 2024 Nicco Kunzmann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Test the commmand line."""
 
 from ics_query.version import version
@@ -16,6 +31,13 @@ def test_check_program_output(io_testcase: IOTestCase):
 def test_version(run):
     """Check the version is displayed."""
     result = run("--version")
+    assert result.exit_code == 0
+    assert version in result.output
+
+
+def test_license(run):
+    """Check the version is displayed."""
+    result = run("--license")
     assert result.exit_code == 0
     assert version in result.output
 
